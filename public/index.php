3d38de4233a11ee00031ea14f0caa649a0ee2541
fikarpakarpikir <fikar.mohammad.6.fm@gmail.com>
2025-11-29 06:40:00 +0700

    fix

--- public/index.php
+++ public/index.php
@@ -28,29 +28,22 @@ require __DIR__ . '/../vendor/autoload.php';
 if (file_exists($maintenance = __DIR__ . '/../storage/framework/maintenance.php')) {
     require $maintenance;
 }
-// jika nyambung dengan flask
-$app = new Illuminate\Foundation\Application(
-    $_ENV['APP_BASE_PATH'] ?? dirname(__DIR__)
-);
-$envFile = Dotenv\Dotenv::createImmutable(dirname(__DIR__,2), 'shared.env');
-// dd($envFile)
-$envFile->safeLoad();
 
-// // Determine correct .env file based on context
-// $envFile = '.env'; // Default
+// Determine correct .env file based on context
+$envFile = '.env'; // Default
 
-// if (PHP_SAPI === 'cli-server') {
-//     // CLI context (e.g., artisan)
-//     // if (env('LARAVEL_ENV') === 'dev') {
-//     $envFile = '.env.dev';
-//     // }
-// } else {
-//     // Web context (e.g., HTTP request)
-//     $host = $_SERVER['HTTP_HOST'] ?? '';
-//     if (in_array($host, ['127.0.0.1', '127.0.0.1:8000'])) {
-//         $envFile = '.env.dev';
-//     }
-// }
+if (PHP_SAPI === 'cli-server') {
+    // CLI context (e.g., artisan)
+    // if (env('LARAVEL_ENV') === 'dev') {
+    $envFile = '.env.dev';
+    // }
+} else {
+    // Web context (e.g., HTTP request)
+    $host = $_SERVER['HTTP_HOST'] ?? '';
+    if (in_array($host, ['127.0.0.1', '127.0.0.1:8000'])) {
+        $envFile = '.env.dev';
+    }
+}
 // dd($envFile);
 
 // // Bootstrap Laravel dan inject env file
@@ -65,8 +58,7 @@ $envFile->safeLoad();
 
 // $kernel->terminate($request, $response);
 // Load the selected .env file
-
-// Dotenv\Dotenv::createImmutable(__DIR__ . '/..', $envFile)->safeLoad(); // safeLoad agar tidak error jika file tidak ada
+Dotenv\Dotenv::createImmutable(__DIR__ . '/..', $envFile)->safeLoad(); // safeLoad agar tidak error jika file tidak ada
 
 // Bootstrap Laravel and handle the request...
 (require __DIR__ . '/../bootstrap/app.php')
